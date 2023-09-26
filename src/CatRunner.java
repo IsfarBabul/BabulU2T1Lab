1ffc4010280e68206c24e91c1c5df65950b417f2
Isfar Babul <isfarb2@nycstudents.net>
2023-09-25 22:31:56 -0400

    cats done

--- src/CatRunner.java
+++ src/CatRunner.java
@@ -1,5 +1,10 @@
 public class CatRunner {
     public static void main(String[] args) {
-        Cat cat1 = new Cat("Tabby", 12, 23.6);
+        Cat cat1 = new Cat("Tabby", 4, 23.6);
+        cat1.introduce();
+        cat1.printCatInfo();
+        Cat cat2 = new Cat("Mittens", 34, 22.7);
+        cat2.printCatInfo();
+        cat2.introduce();
     }
 }
